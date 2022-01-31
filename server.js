1b1fcfbd2af0b36a08c8bc67a0f2c0bbce17841c
Charles A. Hartman <charles@hartman.dev>
2022-01-30 20:53:34 -0800

    mongo split out

--- server.js
+++ server.js
@@ -1,70 +1,19 @@
-// server.js <- adding mongodb
-
-"use strict";
+// server.js <- mongodb test
 
 // Loads the configuration from config.env to process.env
 require('dotenv').config({ path: './config.env' });
 
+const express = require('express');
+const cors = require('cors');
 // get MongoDB driver connection
 const dbo = require('./db/conn');
 
-const express = require('express');
-const favicon = require('serve-favicon');
-const path = require('path');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const { pool } = require('./config');
-
+const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.use(favicon(path.join(__dirname, 'public/images', 'favicon.png')));
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-
-// mongodb additions
 app.use(express.json());
 app.use(require('./routes/record'));
-
-const getBooks = (request, response) => {
-  pool.query('SELECT * FROM books', (error, results) => {
-    if (error) {
-      throw error;
-    }
-    response.status(200).json(results.rows);
-  });
-};
-
-const addBook = (request, response) => {
-  const { author, title } = request.body;
-
-  pool.query(
-    'INSERT INTO books (author, title) VALUES ($1, $2)',
-    [author, title],
-    (error) => {
-      if (error) {
-        throw error;
-      }
-      response.status(201).json({ status: 'success', message: 'Book added.' });
-    }
-  );
-};
-
-const getHome = (request, response) => {
-  response.sendFile(__dirname + '/public/index.html');
-};
-
-app
-  .route('/')
-  .get(getHome);
-
-app
-  .route('/books')
-  // GET endpoint
-  .get(getBooks)
-  // POST endpoint
-  .post(addBook);
 
 // Global error handling
 app.use(function (err, _req, res) {
@@ -80,16 +29,7 @@ dbo.connectToServer(function (err) {
   }
 
   // start the Express server
-  // app.listen(PORT, () => {
-  //   console.log(`Server is running on port: ${PORT}`);
-  // });
-
-  app.listen(process.env.PORT || 3002, () => {
-    console.log(`Server listening`);
+  app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
   });
 });
-
-// Start server
-// app.listen(process.env.PORT || 3002, () => {
-//   console.log(`Server listening`);
-// });
